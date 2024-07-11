317be1b818cbc1eceb33daa816650eb6c940aba8
tengyiG <108433002+tengyiG@users.noreply.github.com>
2024-07-11 15:21:58 -0700

    Update csv_to_pcap.py with imix payload

--- csv_to_pcap.py
+++ csv_to_pcap.py
@@ -10,6 +10,7 @@ from scapy.all import Ether, IP, IPv6, PcapWriter, UDP
 eth_type_ipv6 = 0x86DD
 eth_type_ipv4 = 0x0800
 packets = []
+PART_SUFFIX = '_part'
 
 
 def parse_arguments():
@@ -34,15 +35,21 @@ def parse_arguments():
   )
   parser.add_argument(
       '--flow_label',
-      type=str,
-      default='false',
+      type=bool,
+      default=False,
       help='Whether to generate flow label',
   )
   parser.add_argument(
-      '--payload_size',
-      type=str,
-      default='1472',
-      help='Payload',
+      '--imix',
+      type=bool,
+      default=False,
+      help='IMIX distribution of payload',
+  )
+  parser.add_argument(
+      '--max_file_size',
+      type=int,
+      default=500 * 1024 * 1024,  # Default 500MB
+      help='Maximum file size for PCAP files (in bytes)',
   )
   return parser.parse_args()
 
@@ -120,15 +127,45 @@ def validate_mac_addresses(args):
     args.dst_mac = '55:44:33:22:11:00'
 
 
+def get_payload_size(index, total_rows, imix):
+  if not imix:
+    return 1472
+
+  percent = index / total_rows
+
+  if percent < 0.05:
+    return random.randint(64, 256)
+  elif percent < 0.10:
+    return random.randint(256, 1024)
+  elif percent < 0.20:
+    return random.randint(1024, 2048)
+  elif percent < 0.40:
+    return random.randint(2048, 4192)
+  elif percent < 0.60:
+    return random.randint(4192, 9001)
+  else:
+    return 9001
+
+
+def get_new_pcap_writer(base_pcap_file, part_num):
+  pcap_file = (
+      f'{os.path.splitext(base_pcap_file)[0]}{PART_SUFFIX}{part_num}.pcap'
+  )
+  return PcapWriter(pcap_file, append=True, sync=True), pcap_file
+
+
 def process_files(csv_files, pcap_files, args):
+  print(f'Max file size: {args.max_file_size}')
   for csv_file, pcap_file in zip(csv_files, pcap_files):
     if not os.path.exists(csv_file):
       print(f'This CSV file {csv_file} does not exist')
       continue
 
-    print(f'Processing {csv_file} -> {pcap_file}')
     df = pd.read_csv(csv_file)
-
+    total_rows = len(df)
+    part_num = 1
+    writer, current_pcap_file = get_new_pcap_writer(pcap_file, part_num)
+    print(f'Processing {csv_file} -> {current_pcap_file}')
     for index, row in df.iterrows():
       outer_src_ip = row['src_ip']
       outer_dst_ip = row['dst_ip']
@@ -139,9 +176,7 @@ def process_files(csv_files, pcap_files, args):
         outer_ether = Ether(
             src=args.src_mac, dst=args.dst_mac, type=eth_type_ipv6
         )
-        flow_label: int = 0
-        if args.flow_label == 'true':
-          flow_label = generate_flow_label()
+        flow_label = generate_flow_label() if args.flow_label else 0
         outer_ip = IPv6(src=outer_src_ip, dst=outer_dst_ip, fl=flow_label)
       elif is_valid_ipv4(outer_src_ip) and is_valid_ipv4(outer_dst_ip):
         outer_ether = Ether(
@@ -164,19 +199,30 @@ def process_files(csv_files, pcap_files, args):
         )
         continue
 
-      packet = outer_ether / outer_ip / outer_udp / '0'*int(args.payload)
+      payload_size = get_payload_size(index, total_rows, args.imix)
+      packet = outer_ether / outer_ip / outer_udp / ('0' * payload_size)
       packets.append(packet)
 
-    writer = PcapWriter(pcap_file, append=True, sync=True)
-    for pkt in packets:
-      writer.write(pkt)
-    writer.close()
+      if (
+          os.path.exists(current_pcap_file)
+          and os.path.getsize(current_pcap_file) >= args.max_file_size
+      ):
+        writer.close()
+        print(f'PCAP file saved as {current_pcap_file}')
+        part_num += 1
+        writer, current_pcap_file = get_new_pcap_writer(pcap_file, part_num)
 
-    print(f'PCAP file saved as {pcap_file}')
+      writer.write(packet)
+    writer.close()
+    print(f'PCAP file saved as {current_pcap_file}')
 
 
 def main():
   args = parse_arguments()
+  if args.flow_label:
+    print('--flow_label is activated')
+  if args.imix:
+    print('--imix is activated')
   csv_files, pcap_files = process_csv_and_pcap_files(args)
   validate_mac_addresses(args)
   process_files(csv_files, pcap_files, args)
